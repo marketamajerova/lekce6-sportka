2df1ebd3b3ed0a609af2e47911893c6bde986c80
karolyim <matej.karolyi@gmail.com>
2022-03-22 19:01:12 +0100

    [REFACTOR] project clean (sportka.js)

--- sportka.js
+++ sportka.js
@@ -1,22 +1,15 @@
-// Udělej losování klasické Sportky
-// Máš osudí čísel od 1 do 49
-// Náhodně z tohoto osudí vyber 7 čísel
-// Žádné číslo se nesmí v tahu opakovat
-// (je vyjmuté z osudí, takže už ho nemůžeš znovu vylosovat)
-// Z každého vylosovaného čísla vygeneruj následující HTML kód:
-// <span class="cislo">8</span>
-// ... který pak přidej dovnitř prvku <div id="vyherni-cisla">:
-
+// SPORTKA
 
 let osudi = [];
+let tazenaCisla = [];
+let vyherniCisla = document.querySelector('#vyherni-cisla');
 
+// generovani osudi
 for (let i = 1; i <= 49; i = i + 1) {
     osudi.push(i);
 }
 
-//z osudi vyberu 7 nahodnych cisel
-let tazenaCisla = [];
-
+// z osudi vyberu 7 nahodnych cisel
 for (let i = 0; i < 7; i++) {
     let vyherniIndex = Math.floor(Math.random() * osudi.length);
     let vyherniCislo = osudi[vyherniIndex];
@@ -25,9 +18,7 @@ for (let i = 0; i < 7; i++) {
     osudi.splice(vyherniIndex, 1);
 }
 
-//vypis do HTML
-let vyherniCisla = document.querySelector('#vyherni-cisla');
-
+// vypis do HTML
 for (let j = 0; j < tazenaCisla.length; j++) {
     vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
 }
