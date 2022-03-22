c82ad8ea6a32eedc5c0a1c516cb641e9c96aa12d
Marketa Majerova <marketamajerova.6@gmail.com>
2022-03-22 23:04:16 +0100

    Losovani funkce

--- sportka.js
+++ sportka.js
@@ -1,24 +1,27 @@
-// SPORTKA
 
-let osudi = [];
-let tazenaCisla = [];
-let vyherniCisla = document.querySelector('#vyherni-cisla');
 
-// generovani osudi
-for (let i = 1; i <= 49; i = i + 1) {
-    osudi.push(i);
-}
+function losovani(){
+    // SPORTKA
+    let osudi = [];
+    let tazenaCisla = [];
+    let vyherniCisla = document.querySelector('#vyherni-cisla');
 
-// z osudi vyberu 7 nahodnych cisel
-for (let i = 0; i < 7; i++) {
-    let vyherniIndex = Math.floor(Math.random() * osudi.length);
-    let vyherniCislo = osudi[vyherniIndex];
+    // generovani osudi
+    for (let i = 1; i <= 49; i = i + 1) {
+        osudi.push(i);
+    }
+
+    // z osudi vyberu 7 nahodnych cisel
+    for (let i = 0; i < 7; i++) {
+        let vyherniIndex = Math.floor(Math.random() * osudi.length);
+        let vyherniCislo = osudi[vyherniIndex];
     
     tazenaCisla.push(vyherniCislo);
     osudi.splice(vyherniIndex, 1);
-}
-
-// vypis do HTML
-for (let j = 0; j < tazenaCisla.length; j++) {
-    vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
+}   
+    vyherniCisla.innerHTML = '';
+    // vypis do HTML
+    for (let j = 0; j < tazenaCisla.length; j++) {
+        vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
+    }
 }
